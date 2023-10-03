f19338965a9e5e20d056daf890b5b3956bd9b3b9
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-03 15:29:53 +0200

    add task11

---
+++ task11/blue.js
@@ -0,0 +1,15 @@
+const p = document.querySelector('p');
+
+console.log(p);
+
+p.addEventListener('mouseenter', () => {
+    p.classList.add('blue');
+})
+
+p.addEventListener('mouseleave', () => {
+    p.classList.remove('blue');
+});
+
+p.addEventListener('click', () => { 
+    p.classList.toggle('hightlight');
+});
