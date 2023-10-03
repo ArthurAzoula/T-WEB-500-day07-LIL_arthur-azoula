f6ce898adc89ec8576e8fa9607a3d837a85945b8
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-03 16:46:39 +0200

    Add task13

--- task13/script.js
+++ task13/script.js
@@ -18,7 +18,7 @@ form.addEventListener('submit', (event) => {
 /* second part */
 
 const form2 = document.querySelector('form:last-of-type');
-const select2 = document.querySelector('select:last-of-type');
+const select2 = document.querySelector('form:last-of-type select');
 const search = document.querySelector('form:last-of-type button:first-of-type');
 const reset = document.querySelector('form:last-of-type button:last-of-type');
 const ul = document.querySelector('ul:last-of-type');
@@ -26,15 +26,21 @@ const ul = document.querySelector('ul:last-of-type');
 search.addEventListener('click', (event) => {
     event.preventDefault();
     ul.textContent = '';
+    console.log(select2.value);
     const arraySearch = document.querySelectorAll(`.${select2.value}`);
     if (arraySearch.length === 0) {
-        ul.textContent = 'nothing';
+        ul.textContent = 'nothing...';
         return;
     }
     arraySearch.forEach((element) => {
         const li = document.createElement('li');
         li.textContent = element.textContent;
         ul.appendChild(li);
-        li.classList.add(select.value);
+        li.classList.add(select2.value);
     });
+});
+
+reset.addEventListener('click', (event) => { 
+    event.preventDefault();
+    ul.textContent = '';
 });
